605a1db3b26c78cb274b7d18753f0571cb59a585
Rafael <amauri.rafael33@gmail.com>
2021-11-28 18:44:31 -0300

    feat: Trabalho pronto]

---
+++ Relatorios/Tarefa 07/Resolucao/codigo/programa16.asm
@@ -0,0 +1,17 @@
+.text
+.globl main
+main:
+	lui  $t0, 0x1001
+	addi $t0, $t0, 0x0000 #|1001|0000|
+	lw   $s0, 0($t0)      #s0 = x
+	lw   $s1, 4($t0)      #s1 = y
+	lw   $s2, 8($t0)      #s2 = z
+	div  $s0, $s2         #x/z
+	mflo $t1              #t1 = x/z
+	mult $t1, $s1         #t1*y
+	mflo $t1              #t1 = t1*y	
+	
+.data
+	x:.word 0x186A00
+	y:.word 0x13880
+	z:.word 0x61A80
