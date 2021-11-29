58e02f7b035960a97dff59c5a214bdfd02995480
Rafael <amauri.rafael33@gmail.com>
2021-11-28 22:01:24 -0300

    Done

--- Relatorios/Tarefa 07/Resolucao/codigo/programa14.asm
+++ Relatorios/Tarefa 07/Resolucao/codigo/programa14.asm
@@ -1,8 +1,18 @@
-addi $t0, $zero, 0x1001
-sll $t0, $t0, 16
-
-lw $t0, 0($t0) # Lendo valor da RAM 
-
-andi $t1, $t0, 1 # Fazendo uma AND para descobrir se eh impar ou par
-
-sw $t1, 4($t0) # Guardando na posicao 2 da memoria ( sizeof(int) = 4 )
+.data 
+    valor: .word 4
+    
+.text 
+    ori $t0, $zero, 0x1001 # t0 = 0x1001
+    sll $t0, $t0, 16       # t0 = 0x10010000
+    lw  $s0, 0($t0)         # Pegando primeiro valor da memória
+    
+    andi $t1, $s0, 1       # Verificando se termina com 1, se for é ímpar
+    beqz $t1, PAR          # Se for par saltará direto para
+    sw   $t1, 4($t0)       # Salvar o resultado da operação no primeiro espaço livre da memória
+    j FINALIZAR            # Terminar o programa
+    
+PAR:
+    sw $t1, 4($t0)         # Salvar o resultado da operação no primeiro espaço livre da memória
+    
+FINALIZAR: 
+    J FINALIZAR            # FInalizar o programa
